39530382c6f2f25c9614cc96fba3e8f209730472
Vera <I011018@NGIC.COM>
2020-01-14 11:30:00 -0800

    PTO component starting point

---
+++ src/app/components/CreatePTO/CreatePTO.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-CreatePTO',
+  templateUrl: './CreatePTO.component.html',
+  styleUrls: ['./CreatePTO.component.css']
+})
+export class CreatePTOComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
