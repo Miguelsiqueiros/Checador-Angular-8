df69023e9471252d16936b9c87c0e7397a3351c9
Alfa Venegas <alfa.venegas@cetys.edu.mx>
2020-01-14 11:56:14 -0800

    create empty components for check-in, footer, pto, register, ranking

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,21 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RegisterComponent } from './components/register/register.component';
+import { PtoComponent } from './components/pto/pto.component';
+import { RankingComponent } from './components/ranking/ranking.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { CheckInComponent } from './components/check-in/check-in.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    RegisterComponent,
+    PtoComponent,
+    RankingComponent,
+    FooterComponent,
+    CheckInComponent
   ],
   imports: [
     BrowserModule,
