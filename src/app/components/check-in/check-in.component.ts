ea90f6eca192f62eba273c0aaf1d025bebedc675
Vera <I011018@NGIC.COM>
2020-01-24 11:17:59 -0800

    Added snackbar notifications and some error notifications, there are only some especific error notifications left to add

--- src/app/components/check-in/check-in.component.ts
+++ src/app/components/check-in/check-in.component.ts
@@ -11,32 +11,34 @@ export class CheckInComponent implements OnInit {
   constructor(private _snackBar: MatSnackBar, private checkInObject: CheckInService) { }
   pinValue: number;
   time = new Date();
+  newCheck: any;
   ngOnInit() {
     setInterval(() => {
       this.time = new Date();
     }, 1000);
   }
 
-  SubmitData(){
+  SubmitData() {
     this.checkInObject.checkIn(this.pinValue).subscribe(response => {
-      console.log(response)
-    }, error=>{});;
+      this.newCheck = response;
+      this.SuccessMessage(this.newCheck.name, this.newCheck.date);
+    }, error => { });;
     this.pinValue = null;
   }
 
-  SuccessMessage(name:string, date: Date){
-    this._snackBar.open("Welcome " + name + ",you succesfully checked in at " + date.toString(), "Got It", {
+  SuccessMessage(name: string, date: Date) {
+    this._snackBar.open("Welcome " + name + ", you succesfully checked in at " + date.toString(), "Got It", {
       duration: 4000,
     })
   }
 
-  WrongPinMessage(){
+  WrongPinMessage() {
     this._snackBar.open("The PIN you entered doesn't exist", "", {
       duration: 2000,
     })
   }
 
-  AlreadyCheckedMessage(){
+  AlreadyCheckedMessage() {
     this._snackBar.open("User already checked in", "", {
       duration: 2000,
     })
