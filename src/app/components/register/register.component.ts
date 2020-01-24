478f6a5d56510f54ef48a1a3530343e6b4f26d9f
Vera <I011018@NGIC.COM>
2020-01-23 17:07:59 -0800

    Test snackBar 2

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -28,9 +28,8 @@ export class RegisterComponent implements OnInit {
   SubmitData() {
     this.model.name = this.profileForm.get("fullName").value;
     this.model.image = this.profileForm.get("photoURL").value;
-    this.registerObject.registerName(this.model).subscribe(response => {
-      this.RetrievePin('0000');
-    }, error=>{});
+    this.registerObject.registerName(this.model).subscribe(response => {    }, error=>{});
+    this.RetrievePin('0000');
   }
   
   RetrievePin(pin: string) {
