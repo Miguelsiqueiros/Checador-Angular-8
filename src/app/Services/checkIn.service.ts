111ee551053a9636b29f3aa9e35789f2f6914b33
Vera <I011018@NGIC.COM>
2020-01-28 12:54:28 -0800

    Added fake json server and json file for visual testing updated Check in service to work with it and created the documentation at Readme

--- src/app/Services/checkIn.service.ts
+++ src/app/Services/checkIn.service.ts
@@ -34,19 +34,7 @@ export class CheckInService {
       return this.httpClient.get(`${environment.apiUrl}users/weekly-ladder`);
     }
     else {
-      var DATA = [
-        { name: 'Antonio Nevarez', totalMinutes: 0 },
-        { name: 'Miguel Siqueiros', totalMinutes: 15 },
-        { name: 'Carlos Rondero', totalMinutes: 26 },
-        { name: 'Ernesto Vera', totalMinutes: 39 },
-        { name: 'Luis Maldonado', totalMinutes: 52 },
-        { name: 'Felipe Padilla', totalMinutes: 62 },
-        { name: 'Jesus Campos', totalMinutes: 68 },
-        { name: 'Pedro Esparza', totalMinutes: 76 },
-        { name: 'Jemmy Preciado', totalMinutes: 83 },
-        { name: 'Alfa Venegas', totalMinutes: 92 },
-      ];
-      return DATA;
+      return this.httpClient.get(`http://localhost:3000/weekly`);
     }
   }
 
@@ -56,19 +44,7 @@ export class CheckInService {
       return this.httpClient.get(`${environment.apiUrl}users/daily-ladder`);
     }
     else {
-      var DATA = [
-        { name: 'Antonio Nevarez', totalMinutes: 0, pto: false },
-        { name: 'Jemmy Preciado', totalMinutes: 1, pto: false },
-        { name: 'Ernesto Vera', totalMinutes: 2, pto: false },
-        { name: 'Carlos Rondero', totalMinutes: 3, pto: false },
-        { name: 'Jesus Campos', totalMinutes: 5, pto: true },
-        { name: 'Felipe Padilla', totalMinutes: 6, pto: false },
-        { name: 'Luis Maldonado', totalMinutes: 6, pto: false },
-        { name: 'Pedro Esparza', totalMinutes: 7, pto: false },
-        { name: 'Miguel Siqueiros', totalMinutes: 8, pto: false },
-        { name: 'Alfa Venegas', totalMinutes: 9, pto: false },
-      ];
-      return DATA;
+      return this.httpClient.get(`http://localhost:3000/daily`);
     }
   }
 
